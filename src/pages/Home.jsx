faf2d0f8e754ec25a30a3ca99dd5bd38a82b64c0
Kalaugus <josemagana90@yahoo.com>
2024-02-01 16:03:16 -0800

    done for the day working still

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,14 @@ const Home = () => {
     <section className='w-full h-screen relative'>
       <Canvas
        className='w-full h-screen bg-transparent'
-      camera={{ near: 0.1, far: 1000, }
-
-       >
+      camera={{ near: 0.1, far: 1000, }} >
        <Suspense fallback={<Loader/>}>
+        <directionalLight/>
+        <ambientLight/>
+        <spotLight/>
+        <hemisphereLight/>
+       </Suspense>   
+      
 
       </Canvas>
 
